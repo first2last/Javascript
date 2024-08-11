68694e6f3e3979b009ad2f9ce8ad7d46cea78e6a
Vishesh Mishra <149947608+first2last@users.noreply.github.com>
2024-08-11 23:08:48 +0000

    new one

---
+++ basics/data_types.js
@@ -0,0 +1,13 @@
+let Name = "vishesh" ;
+let age = 20 ;
+let smart = true  ; 
+let future = undefined ; 
+let past = null ; 
+
+console.log(typeof age) ; 
+console.log(typeof Name) ;
+console.log(typeof smart) ; 
+console.log(typeof future) ; 
+console.log(typeof past) ; 
+
+console.log([typeof age , typeof Name , typeof smart  , typeof future , typeof past]) ; 
---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const id = 1 ;
+var name = "Vishesh" ;
+let password = "@2023021266" ;
+
+city = "Farrukhabad" ; 
+
+console.table([id , name , password ,city]) ; 
