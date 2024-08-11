c5e17fb731f628eba382270b26984a3d698c3161
Vishesh Mishra <149947608+first2last@users.noreply.github.com>
2024-08-11 23:43:32 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,31 @@
+let age = 45 ; 
+
+
+// to string //
+
+let stringAge = String(age) ; 
+console.log(typeof(stringAge)) ; 
+
+// to number//
+
+let me = "123VISH" ; 
+let you = "23" ; 
+
+let NumberMe = Number(me) ; 
+let NumberYou = Number(you) ; 
+
+console.log([NumberMe , NumberYou]) ; 
+console.log([typeof NumberMe , typeof NumberYou]) ;
+
+// to boolean //
+
+  let us = "Yes" ; 
+  let future = "" ; 
+  let prob = 0.5  ; 
+
+  console.log(typeof prob) ; 
+  let booleanUs = Boolean(us) ; 
+  let booleanFuture = Boolean(future) ; 
+  let booleanProb = Boolean(prob) ; 
+
+  console.log([ typeof booleanFuture , typeof booleanProb , typeof booleanUs]) ; 
