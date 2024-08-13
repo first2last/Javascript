f2d4bf31157423299612cf17e8ac253ef965e2ee
Vishesh Mishra <149947608+first2last@users.noreply.github.com>
2024-08-13 23:49:32 +0000

    14/08/24

---
+++ basics/date.js
@@ -0,0 +1,12 @@
+let MyDate =  new Date() ; 
+
+console.log(MyDate);
+console.log(MyDate.toDateString());
+console.log(MyDate.toISOString());
+console.log(MyDate.toJSON());
+console.log(MyDate.toLocaleDateString());
+console.log(MyDate.toLocaleString());
+
+
+
+
---
+++ basics/maths.js
@@ -0,0 +1,14 @@
+const salary = 23090.34 ; 
+
+console.log(Math.floor(salary));
+
+const GDP = 13243009900 ; 
+
+console.log(GDP.toLocaleString("en-IN")) ;
+
+const max = 20 ; 
+const min = 10 ;
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min) ; 
+
+
