ccc2eeef8bf5fe23ed544c3aec9dd45ccfb26015
Vishesh Mishra <149947608+first2last@users.noreply.github.com>
2024-08-18 11:21:09 +0000

    objects

---
+++ basics/objects.js
@@ -0,0 +1,26 @@
+const myobj = {
+    Name: "Vishesh",
+    Location: "Fbd",
+    age: 20,
+    Engaged: false,
+    Test: true
+}
+
+const newobj = {
+    goal: "Brahma",
+    focus: true,
+    difficulty: "Hard"
+}
+
+// console.log(myobj.Name, myobj.Location, myobj.age, myobj.Engaged, myobj.Test);
+// console.log(newobj.goal, newobj.focus, newobj.difficulty);
+
+// console.log(myobj["Name"] ) ;
+
+
+myobj.Engaged = "never" ;
+
+console.log(myobj["Engaged"]);
+
+myobj
+
